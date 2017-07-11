ed39bc4a61c642b8b848a90ff215019fad5c904d
cristian-castrillon <cristian.castrillon@gmail.com>
2017-07-11 18:09:32 -0500

    [ADD] Implementó las funcionalidades de ver, crear, actualizar y eliminar post, con sus respectivas vistas

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,48 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to(root_path, notice: 'Post publicado con éxito')
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to(root_path, notice: 'Post ha sido modificado con éxito')
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to(root_path, notice: 'Post ha sido eliminado con éxito')
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+    def find_post
+      @post = Post.find(params[:id])
+    end
 end
