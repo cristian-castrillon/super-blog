49ba4e9a8391cd9b6c089fcf7ead648c9914d22b
cristian-castrillon <cristian.castrillon@gmail.com>
2017-07-24 17:31:26 -0500

    [ADD] Support for markdown on post body

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,9 @@ module PostsHelper
   def form_title
     @post.new_record? ? 'Publicar Post' : 'Modificar Post'
   end
+
+  def markdown(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, no_intra_emphasis: true, fenced_code_blocks: true, disable_indented_code_blocks: true)
+    return markdown.render(text).html_safe
+  end
 end
